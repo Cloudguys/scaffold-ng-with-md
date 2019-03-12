43f80e5162bd3499572e7f568ddd339a51a1502e
Bin <dengbin80@live.com>
2019-03-13 00:40:39 +0800

    feat: basic layout improvements (#6)
    changes to this commit:
    - add sidenav menu component
    - fix width and heigh isuess of sidenav and content container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +16,7 @@ import {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
 
     SharedModule,
   ],
